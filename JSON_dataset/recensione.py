1bf43c0f1dec0e6997169d227cfa5b8f26ced937
Janat <250729@studenti.unimore.it>
2023-04-24 21:42:57 +0200

    " aggiornamento recensione.py  "

--- JSON_dataset/recensione.py
+++ JSON_dataset/recensione.py
@@ -11,13 +11,13 @@ b = "Great. I love Amazon."
 print(len(b))
 
 class recensione:
-    def __init__(self, pk, reviewer_name, review_text, asin, title, main_cat):
-        self.pk = pk
-        self.reviewerName = reviewer_name
-        self.reviewText = review_text
-        self.asin = asin
-        self.title = title
-        self.main_cat = main_cat
+    def __init__(self,*args):
+        self.pk = args[0]
+        self.reviewerName = args[1]
+        self.reviewText = args[2]
+        self.asin = args[3]
+        self.title = args[4]
+        self.main_cat = args[5]
 
     def __iter__(self):
         return iter([self.pk, self.reviewerName, self.reviewText, self.asin, self.title, self.main_cat])
@@ -25,3 +25,5 @@ class recensione:
     def __repr__(self):
         return f"{self.pk} | {self.reviewerName} | {self.reviewText} | {self.asin} | {self.title}  | {self.main_cat} "
 
+    def get_pk(self):
+        return self.pk
