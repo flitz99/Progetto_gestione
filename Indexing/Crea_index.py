92dfca3ee3af87264d60c482083ed0cdbdb51b24
Janat <250729@studenti.unimore.it>
2023-05-07 12:58:35 +0200

    " fix bug"

--- Indexing/Crea_index.py
+++ Indexing/Crea_index.py
@@ -1,19 +1,18 @@
 import os
-import Database
-import Indexing
+from Database import Database
+from Indexing import Indexer
 
 def crea_index_se_non_esiste():
     is_index_dir = os.path.exists('../Indexing/indexdir_2.0')
 
     if not is_index_dir:
-        db = Database.Database('../JSON_dataset/dataset_sentiment.csv')
+        db = Database('../JSON_dataset/dataset_sentiment.csv')
         db.init_DB()
 
-        index = Indexing.Indexer(db)
+        index = Indexer(db)
         index.create_index()
 
         if index.ix.is_empty() == True:
             print("fallimento")
 
 
-crea_index_se_non_esiste()
