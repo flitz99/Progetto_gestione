c21741300543f8b5dca736c59f2cf5388f625d9e
Janat <250729@studenti.unimore.it>
2023-05-08 14:49:28 +0200

    " refractor "

--- Search_and_Result/Start.py
+++ Search_and_Result/Start.py
@@ -2,30 +2,33 @@ from Searcher import *
 from Results import *
 from Indexing_Database.Crea_index import *
 
-
 """
 ho commentato per rendere la fase di testing più veloce senza dover fare sempre input
 """
 
 starter = Start_indexing()
 
-search= Index_Searcher()
+search = Index_Searcher()
 search.inizializza()
 
-#query= input("Inserire query: ")
-# sentiment_analyzer = input("Inserire analizzatore sentimento: ")
-# sentiment=input("Inserire sentimento: ")
+# query= input("Inserire query: ")
+sentiment_analyzer = input("Inserire analizzatore sentimento: ")
 
-query = "good book"
-results= search.submit_query(query,results_threshold=10,ricerca_precisa=False)
+if sentiment_analyzer == "Vader" or sentiment_analyzer == "TextBlob":
+    print("positivo , negativo o neutrale ?")
+    sentiment = input("Inserire sentimento: ")
+elif sentiment_analyzer == "distilroberta":
+    print("joy, ")
+    sentiment = input("Inserire tipo sentimento: ")
 
-for cont,r in enumerate(results):
-    print(cont,r)
+query = "good reads"
+results = search.submit_query(query, results_threshold=5, ricerca_precisa=False)
 
+for cont, r in enumerate(results):
+    print(cont, r)
 
 """"
 test per vedere se funziona searcher
 """
 # print("eseguo test")
 # search.Test_Controllo_Index("segment")
-
