63be195803e5e26ebcc38a6effc5b6bba1d7d83d
Janat <250729@studenti.unimore.it>
2023-05-08 15:57:25 +0200

    Changes by filipporeggiani

--- Search_and_Result/Start.py
+++ Search_and_Result/Start.py
@@ -8,23 +8,33 @@ ho commentato per rendere la fase di testing più veloce senza dover fare sempre
 
 starter = Start_indexing()
 
+#Richiesta e ricerca query inserita nell'index
+query= input("Inserire query: ")
+print("vuoi usare BM25F o TF_IDF? (scelta omessa o errata viene usato TF_IDF come standard)")
+scoring_method=input("Inserire meccanismo di scoring: ")
+search = Index_Searcher(algoritmo_di_ricerca=scoring_method)
 
 
-# query= input("Inserire query: ")
+results = search.submit_query(query, results_threshold=5, ricerca_precisa=False) #Ricerca query nell'index
+
+#Risultati prima della sentiment
+for cont, r in enumerate(results):
+    print(cont, r)
+
+
+#richiesta analizzatore sentiment
 sentiment_analyzer = input("Inserire analizzatore sentimento: ")
 
 if sentiment_analyzer == "Vader" or sentiment_analyzer == "TextBlob":
     print("positivo , negativo o neutrale ?")
     sentiment = input("Inserire sentimento: ")
 elif sentiment_analyzer == "distilroberta":
-    print("joy, ")
+    print("joy, surprise, neutral, fear, sadness, anger, disgust? ")
     sentiment = input("Inserire tipo sentimento: ")
 
-query = "good reads"
-results = search.submit_query(query, results_threshold=5, ricerca_precisa=False)
-
-for cont, r in enumerate(results):
-    print(cont, r)
+#Risultati dopo sentiment
+#for cont, r in enumerate(results):
+#    print(cont, r)
 
 """"
 test per vedere se funziona searcher
