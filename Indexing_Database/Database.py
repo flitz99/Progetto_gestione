4a39493f92e5d19c6fb24a10d75c5a27326b8c13
Janat <250729@studenti.unimore.it>
2023-05-14 15:11:54 +0200

    " :bulb: aggiunti commenti per Database.py e Indexing.py "

--- Indexing_Database/Database.py
+++ Indexing_Database/Database.py
@@ -1,8 +1,11 @@
 import csv
 from JSON_dataset import recensione as rec
-
+import random
 
 class Database:
+    '''
+    Classe che gestisce il caricamento dei dati da file CSV dentro python
+    '''
 
     def __init__(self, csv_file):
 
@@ -12,6 +15,10 @@ class Database:
         self.lista_recensioni = []  # lista
 
     def init_DB(self):
+        '''
+        carica il file CSV sepcificato alla inizializzazione e lo trasforma in una lista di dati dentro un altra lista
+        :return:
+        '''
 
         for index, riga in enumerate(self.csv_reader):  # enumerate, serve per avere un contatore del elemento attuale
 
@@ -26,21 +33,37 @@ class Database:
                 self.lista_recensioni.append(temp)  # aggiungo la recensione alla lista
 
     def Test_numero_ultima_recensione(self):
+        '''
+        Piccolo test per vedere quante recensioni ci sono
+        :return:
+        '''
         temp = self.lista_recensioni[-1]
         print(temp.get_pk())  # controllo pk del ultimo elemento nella lista
 
     def Test_Conta_caratteri_totali(self, lista):
+        '''
+        Piccolo test usato per valutare se potessimo usare open AI come sentiment analyzer
+        calcoliamo la lunghezza totale dei caratteri di tutte le recensioni
+
+        OPEN-AI accetta
+        :param lista: sono tutte le recensioni
+        :return: ritorna il colcolo
+        '''
 
         for l in self.lista_recensioni:
             temp = l
             lista.append(temp.count_review())
 
     def Benchmark_mode(self):
+        '''
+        per il benchmark filtriamo la lista  e prendiamo solamente 100 recensioni di film
+        :return: nullo
+        '''
         print("procedo a prendere 100 recensioni in modo casuale")
-        import random
-        #solo_film = filter(lambda l: l[5] == 'Movies and TV', self.lista_recensioni)
+
         solo_film = [l for l in self.lista_recensioni if l[5] == 'Movies and TV']
-
-        updatedlist = random.sample(solo_film, 100)
-
-        self.lista_recensioni = updatedlist
+        if len(solo_film) >= 100:
+            updatedlist = random.sample(solo_film, 100)
+            self.lista_recensioni = updatedlist
+        else:
+            print("elementi non bastano")
